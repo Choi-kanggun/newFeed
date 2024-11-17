80c1f3ec91512cb1b56a263502edf0d61d61e1f8
Choi-kanggun <kg4889@gmail.com>
2024-11-17 17:22:12 +0900

    merge: feat/detail into dev

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -4,9 +4,7 @@ import Login from '../pages/Login';
 import SignUp from '../pages/SignUp';
 import Home from '../pages/Home';
 import Detail from '../pages/Detail';
-
 import MyPost from '../pages/MyPost';
-
 import CreatePostPage from '../pages/CreatePostPage';
 import CorrectionPage from '../pages/CorrectionPage';
 
