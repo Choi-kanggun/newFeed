8d250e02ef16de40df14e62e0c9ec0bf6e5c4b0f
Hobin-joo <wnghqls12@naver.com>
2024-11-19 20:38:18 +0900

    m

--- src/components/myPost/Correction.jsx
+++ src/components/myPost/Correction.jsx
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { Btn, Div, Div2, Form } from '../../styles/post';
 import { supabase } from '../../supabase/supabaseClient';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Correction = () => {
   const [song_url, setSong_Url] = useState('');
   const [content, setContent] = useState('');
   const [title, setTitle] = useState('');
+  const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
-      let { data, error } = await supabase.from('posts').select('*');
-      console.log(data);
-      setSong_Url(data.song_url);
-      setContent(data.content);
-      setTitle(data.title);
+      let { data, error } = await supabase.from('posts').select('*').eq('id', id);
+      console.log('data', data);
+      setSong_Url(data[0].song_url);
+      setContent(data[0].content);
+      setTitle(data[0].title);
     };
     fetchData();
   }, []);
 
   const handelSaveEdit = async (e) => {
-    const { data, error } = await supabase.from('posts').update({ song_url, content, title }).eq('id', user.id);
+    e.preventDefault();
+    const { data, error } = await supabase.from('posts').update({ song_url, content, title }).eq('id', id);
+    navigate('/mypost');
   };
 
   const handelSongUrlChange = (e) => {
