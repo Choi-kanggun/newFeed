9c2365e1261ee78f18fa50688cdeb77cd5180e6d
zi0w <zzz13588@naver.com>
2024-11-20 20:59:35 +0900

    <add: likes.js, PostCard.js> + <update: Header.js, PostList.js, PostCard.js>

--- src/styles/PostList.js
+++ src/styles/PostList.js
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 export const PostListContainer = styled.div`
   background-color: #f8f9fa;
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(190px, 1fr));
   grid-auto-rows: min-content;
-  gap: 2rem;
+  gap: 4rem;
   padding: 2rem 8rem 2rem 8rem;
   flex-grow: 1;
 `;
